a05491a3c8f980bdea187669810f7b7b46de1cd0
redaphid <iam@aaronherres.com>
2019-03-08 13:38:13 -0700

    an ugly, broken test in an ugly, broken language for ugly, broken people

--- game/game.go
+++ game/game.go
@@ -1,8 +1,13 @@
 package game
 
+import (
+	"math/rand"
+)
+
 //Player represents the data of a single player.
 type Player struct {
-	MyCards []int8
+	MyCards    []int
+	TheirCards []int
 }
 
 // Game represents an instance of the game
@@ -23,15 +28,19 @@ func New() Game {
 
 //NewPlayer returns a new Player instance
 func NewPlayer() Player {
-	return Player{
-		MyCards: []int8{
-			0,
-			0,
-			0,
-			0,
-			0,
-		},
+	cardCount := 10
+	initialHandCount := cardCount / 2
+
+	p := Player{
+		MyCards:    make([]int, initialHandCount),
+		TheirCards: make([]int, 0),
 	}
+
+	for i := 0; i < initialHandCount; i++ {
+		p.MyCards[i] = rand.Intn(cardCount) + 1
+	}
+
+	return p
 }
 
 func (g *Game) String() string {
--- game/game_test.go
+++ game/game_test.go
@@ -1,6 +1,8 @@
 package game_test
 
 import (
+	"math/rand"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,17 +13,22 @@ var _ = Describe("Game", func() {
 	var (
 		g game.Game
 	)
+
 	BeforeEach(func() {
+		rand.Seed(0)
 		g = game.New()
 	})
+
 	It("Should instantiate", func() {
 		Expect(g).NotTo(BeNil())
 	})
+
 	It("Should instantiate 2 players by default", func() {
 		g := game.New()
 		players := g.Players
 		Expect(len(players)).To(Equal(2))
 	})
+
 	Describe("Player", func() {
 		var (
 			p game.Player
@@ -32,5 +39,32 @@ var _ = Describe("Game", func() {
 		It("Should instantiate a player with 5 cards by default", func() {
 			Expect((len(p.MyCards))).To(Equal(5))
 		})
+
+		It("Should populate the hand with numbers between 1-10", func() {
+			for i := 0; i < 5; i++ {
+				Expect(p.MyCards[i] > 0).To(BeTrue())
+				Expect(p.MyCards[i] < 11).To(BeTrue())
+			}
+		})
+
+		It("Should not populate the hand with the same card twice", func() {
+
+			for i := 0; i < 1000; i++ {
+				p2 := game.NewPlayer()
+				for j := 0; j < 5; j++ {
+					for k := 0; k < 5; k++ {
+						if i == j {
+							continue
+						}
+						Expect(p2.MyCards[j]).NotTo(Equal(p2.MyCards[k]))
+					}
+				}
+			}
+		})
+
+		It("Should instantiate a player with no opponent cards by default", func() {
+			Expect((len(p.TheirCards))).To(Equal(0))
+		})
+
 	})
 })
