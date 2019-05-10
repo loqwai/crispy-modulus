67854cafa734de1d4e7a16ce70f96986b5c357b5
redaphid <iam@aaronherres.com>
2019-05-10 16:55:36 -0700

    a custom boolean class. Yep

--- Assets/src/GameSystem.cs
+++ Assets/src/GameSystem.cs
@@ -3,11 +3,12 @@ using Unity.Mathematics;
 using UnityEngine;
 public class GameSystem : ComponentSystem
 {
-    protected override void OnCreate() {
-        Debug.Log("I was born");
-    }
+
     protected override void OnUpdate()
     {
+        Entities.ForEach((ref Game game) => {
+            game.shouldStart = false;      
+        });
         // Entities.ForEach processes each set of ComponentData on the main thread. This is not the recommended
         // method for best performance. However, we start with it here to demonstrate the clearer separation
         // between ComponentSystem Update (logic) and ComponentData (data).
