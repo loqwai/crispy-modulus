e383f4732158e0c9f29cca3c38d45529089129b1
Roy van de Water <roy.vwater@gmail.com>
2019-05-10 16:23:05 -0700

    Cards are added to the hand on Click

---
+++ Assets/DrawCardOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DrawCardOnClick : MonoBehaviour {
+  public Transform Hand;
+
+  void OnMouseDown() {
+    if (children.Length == 0) return;
+
+    var card = children.First();
+    card.SetParent(Hand);
+  }
+
+  Transform[] children {
+    get {
+      var children = new List<Transform>();
+      foreach (Transform child in transform) {
+        children.Add(child);
+      }
+      return children.ToArray();
+    }
+  }
+
+}
