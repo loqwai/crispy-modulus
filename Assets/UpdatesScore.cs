c25eaf8ab87063fe4fad22df1801e86ef66c2ab0
Roy van de Water <roy.vwater@gmail.com>
2019-05-10 17:15:35 -0700

    Disable Burst compiler for Android, add score display

---
+++ Assets/UpdatesScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatesScore : MonoBehaviour {
+  public Text ScoreDisplay;
+
+  void Update() {
+    ScoreDisplay.text = $"Score: {score}";
+  }
+
+  int score {
+    get {
+      return cards.Select(c => c.Value).Sum();
+    }
+  }
+
+  Card[] cards {
+    get {
+      return GetComponentsInChildren<Card>();
+    }
+  }
+}
