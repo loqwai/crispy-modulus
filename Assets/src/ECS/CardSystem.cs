f14976bc5117b5e95a56ec3a648f65560c93f666
redaphid <iam@aaronherres.com>
2019-05-15 01:21:53 -0700

    it's getting easier, but seriously #fuckthedocs on this project

---
+++ Assets/src/ECS/CardSystem.cs
@@ -0,0 +1,43 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+
+using OurECS;
+namespace OurECS {
+  [UpdateBefore(typeof(GameSystem))]
+  public class CardSystem : ComponentSystem {            
+    protected EntityArchetype cardArchetype;
+
+    void onCreate() {
+      cardArchetype = EntityManager.CreateArchetype(typeof(Card));
+    }
+    protected void openANewDeckJustLikeVegas() {
+      var query = GetEntityQuery(typeof(Card));
+      var cardEntities = query.ToEntityArray(Allocator.TempJob);
+      foreach(var e in cardEntities ) {
+        PostUpdateCommands.DestroyEntity(e);
+      }      
+    }
+
+    protected void dealCards(Game game) {
+      Entities.ForEach( (ref Player p) => {
+        for(int i = 0; i < game.cardCount; i++) {
+          PostUpdateCommands.CreateEntity(cardArchetype);   
+          PostUpdateCommands.AddComponent(new Card{value=i});       
+        }
+      });      
+    }
+
+    protected override void OnUpdate() {
+      if(!HasSingleton<Game>()) return;
+
+      var game = GetSingleton<Game>();      
+      if (game.action == Game.Actions.Start) {
+          openANewDeckJustLikeVegas();
+          dealCards(game);          
+      }            
+    }
+  }
+}
